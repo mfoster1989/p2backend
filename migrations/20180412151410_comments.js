733a0a3dab096d40e86204fa30d7dee88c540edf
Malcolm Foster <malcolmfoster@gmail.com>
2018-04-16 10:17:07 -0600

    :rocket:

--- migrations/20180412151410_comments.js
+++ migrations/20180412151410_comments.js
@@ -2,7 +2,7 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable("comments", function (table) {
         table.increments("id").primary()
-        table.string("media")
+        table.text("media")
         table.text("comments")
     })
 };
