0e16ed4658692ec507a31374231ee38a39082357
Malcolm Foster <malcolmfoster@gmail.com>
2018-04-16 13:04:33 -0600

    :rewind: had trouble renaming comments to reviews, reverting

---
+++ seeds/02_comments.js
@@ -0,0 +1,14 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('comments').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('comments').insert([
+        { id: 1, media: 'Star Trek Beyond', comments: "tried too hard" },
+        { id: 2, media: 'Star Trek Nemesis', comments: "horrible way to end a franchise" }
+      ]);
+    })
+    .then(function () {
+      return knex.raw("ALTER SEQUENCE comments_id_seq RESTART WITH 2")
+    })
+};
